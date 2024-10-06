2276523c05b32b030644e2a007bd8dd9be9633d6
heegu1127 <slhgslhg59@gmail.com>
2024-10-06 15:20:21 +0900

    commit 6

--- camera_test.py
+++ camera_test.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 # 카메라 장치 번호 설정 (기본적으로 첫 번째 카메라를 0번으로 설정)
-camera_port = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index0"
+camera_port = "/dev/v4l/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.1:1.0-video-index1"
 
 # 이미지가 저장될 디렉터리
 save_dir = "/media/pi/CAMERA_SD"
